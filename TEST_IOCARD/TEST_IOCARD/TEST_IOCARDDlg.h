0a4bbb42a425f42124a62a112b4dcebf90a79fdb
zhaoxiaochun-art <337416641@qq.com>
2020-05-09 11:28:19 +0800

    backup

---
+++ TEST_IOCARD/TEST_IOCARD/TEST_IOCARDDlg.h
@@ -0,0 +1,40 @@
+﻿
+// TEST_IOCARDDlg.h: 头文件
+//
+#include "IoCard.h"
+#pragma once
+
+
+// CTESTIOCARDDlg 对话框
+class CTESTIOCARDDlg : public CDialogEx
+{
+// 构造
+public:
+	CTESTIOCARDDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_TEST_IOCARD_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+	CioControl *iocard;
+// 实现
+protected:
+	HICON m_hIcon;
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButtonReadtimer();
+	afx_msg void OnBnClickedButtonReadio();
+	afx_msg void OnBnClickedButtonOpen();
+	afx_msg void OnBnClickedButtonInit();
+	afx_msg void OnBnClickedButtonStart();
+	afx_msg void OnBnClickedButtonStop();
+};
---
+++ TEST_IOCARD/TEST_IOCARD/resource.h
@@ -0,0 +1,31 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 TESTIOCARD.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_TEST_IOCARD_DIALOG          102
+#define IDR_MAINFRAME                   128
+#define IDC_BUTTON_OPEN                 1000
+#define IDC_BUTTON_INIT                 1001
+#define IDC_BUTTON_START                1002
+#define IDC_BUTTON4                     1003
+#define IDC_BUTTON5                     1004
+#define IDC_BUTTON6                     1005
+#define IDC_BUTTON_ReadTimer            1006
+#define IDC_EDIT1                       1007
+#define IDC_BUTTON_ReadIO               1008
+#define IDC_EDIT_INDEXIN                1009
+#define IDC_BUTTON_STOP                 1010
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
